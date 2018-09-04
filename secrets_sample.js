8fc734dc6c489dd0ad59eb1c3e553a24e22e02f5
Maurits van Altvorst <mvanaltvorst@icloud.com>
2018-09-04 17:39:37 +0200

    Remove unused dependencies

---
+++ secrets_sample.js
@@ -0,0 +1,3 @@
+exports.mnemonic = ""; // private key mnemonic used by TruffleHDWallet
+exports.infura_endpoint = "https://ropsten.infura.io/v3/<YOUR_SECRET>"; // infura endpoint with API key included
+
